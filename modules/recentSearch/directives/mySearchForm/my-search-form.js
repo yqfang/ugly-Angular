e6429a0831de339172967715fcdd31f326e8cdda
yqfang <404762352@qq.com>
2015-02-10 14:19:49 +0800

    #9.add some comments

--- modules/recentSearch/directives/mySearchForm/my-search-form.js
+++ modules/recentSearch/directives/mySearchForm/my-search-form.js
@@ -9,13 +9,13 @@ angular.module("myApp")
                     vm.form = {};
                 }],
                 controllerAs: 'myGroupedTableSearchForm',
-                require: ['^recentSearchPage', 'myGroupedTableSearchForm'],
+                require: ['?^recentSearchPage', 'myGroupedTableSearchForm'],
                 link: function(scope, element, attr, ctrls){
                     //proxy the search action to the upper-dom directive
                     var recentSearchPage = ctrls[0];
                     var myGroupedTableSearchForm = ctrls[1];
                     myGroupedTableSearchForm.search = function(){
-                        myGroupedTableSearchForm && recentSearchPage.search(myGroupedTableSearchForm.form);
+                        recentSearchPage && recentSearchPage.search(myGroupedTableSearchForm.form);
                     }
 
                 }
